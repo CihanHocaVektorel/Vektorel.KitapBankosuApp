36fc90658947dd3690b161c4af7b69740b5fb036
vektorel <vektorel@LAB3-OGR>
2019-12-21 12:23:28 +0200

    Disconnected İşlemleri

--- Vektorel.KitapBankosuApp/frmKitapListe.cs
+++ Vektorel.KitapBankosuApp/frmKitapListe.cs
@@ -8,20 +8,59 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Vektorel.KitapBankosu.BLL;
+using Vektorel.KitapBankosu.Models;
 
 namespace Vektorel.KitapBankosuApp
 {
     public partial class frmKitapListe : Form
     {
+        DataTable dt;
         public frmKitapListe()
         {
             InitializeComponent();
+            grdYazarlar.AutoGenerateColumns = false;
         }
 
         private void FrmKitapListe_Load(object sender, EventArgs e)
         {
             YazarBL yz = new YazarBL();
-            grdYazarlar.DataSource = yz.YazarTablosu();
+            dt = yz.YazarTablosu();
+            grdYazarlar.DataSource = dt;
+        }
+
+        private void BtnSaveChanges_Click(object sender, EventArgs e)
+        {
+            YazarBL yb = new YazarBL();
+            foreach (DataRow item in dt.Rows)
+            {
+                Yazar yz = new Yazar();
+                if (item.RowState != DataRowState.Deleted)
+                {                    
+                    yz.Ad = item["Ad"].ToString();
+                    yz.Soyad = item["Soyad"].ToString();
+                    yz.DTar = Convert.ToDateTime(item["DTar"]);
+                    if (item["OlumTarih"].ToString() != "")
+                    {
+                        yz.OlumTarih = Convert.ToDateTime(item["OlumTarih"]);
+                    }
+                    yz.Yazarid = Convert.ToInt32(item["YazarId"]);
+                }
+               
+                switch (item.RowState)
+                {
+                    case DataRowState.Added:
+                        yb.YazarEkle(yz);
+                        break;
+                    case DataRowState.Deleted:
+                        yb.YazarSil(Convert.ToInt32(item["YazarId",DataRowVersion.Original]));
+                        break;
+                    case DataRowState.Modified:
+                        yb.YazarGuncelle(yz);
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
